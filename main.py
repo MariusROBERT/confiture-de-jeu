7fc2e64c56a039fb219124087bd5f70b79603818
Thibault mathian <thibault.mathiant@free.fr>
2022-02-07 10:55:10 +0100

    main base

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+from multiprocessing import Event
 import pygame
+import sys
+
+
+pygame.init()
+
+size = width, height = 320, 240
+
+screen = pygame.display.set_mode(size)
+
+
+def event_loop(event: pygame.event.Event):
+    if event.type == pygame.QUIT:
+        sys.exit()
+
+
+while 1:
+    for event in pygame.event.get():
+        event_loop(event)
+
+    # pygame.display.flip()
