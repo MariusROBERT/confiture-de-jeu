33259ab2f11f7e5a7e4c3e7ea32d9d83afcd5ed4
lefranni <nicolas.lefranc1@etu.univ-grenoble-alpes.fr>
2022-02-07 12:34:57 +0100

    joli patate

--- personnages/Patate.py
+++ personnages/Patate.py
@@ -9,16 +9,20 @@ class Patate():
         self.image_patate = pygame.image.load("../images/patate.png")
         self.image_pousse= pygame.image.load("../images/pousse_patate.png")
         self.size=CASE_SIZE
+        
         x_pousse=random.randint(0,NB_ELEM_X)
         y_pousse=random.randint(0,NB_ELEM_Y)
         pos_pousse=x_pousse*CASE_SIZE,y_pousse*CASE_SIZE
+
         while pos_pousse not in TOURS :
             x_pousse=random.randint(0,NB_ELEM_X)
             y_pousse=random.randint(0,NB_ELEM_Y)
             pos_pousse=x_pousse*CASE_SIZE,y_pousse*CASE_SIZE
+
         x_patate=randrange(x_pousse-1,x_pousse+2)
         y_patate=randrange(y_pousse-1,y_pousse+2)
         pos_patate=x_patate*CASE_SIZE,y_patate*CASE_SIZE
+
         while pos_patate not in TOURS and pos_patate[1] < SIZE[1] and pos_patate[2] < SIZE[2] :
             x_patate=randrange(x_pousse-1,x_pousse+2)
             y_patate=randrange(y_pousse-1,y_pousse+2)
