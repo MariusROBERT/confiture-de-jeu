3570414454010d308181aabee6269d83c44ad5de
Nico <45742039+nico-vrn@users.noreply.github.com>
2022-02-07 16:14:31 +0100

    fail

--- test.py
+++
@@ -1,9 +0,0 @@
-from constantes import TOURS, NB_ELEM_X, NB_ELEM_Y, CASE_SIZE, SIZE
-import random
-from random import randrange
-
-test=(400,160)
-for i in range(len(TOURS)):
-    while test in TOURS[i]:
-        test=(randrange(0,NB_ELEM_X),randrange(0,NB_ELEM_Y))
-    print(test)
