d35af24eef97acef19a0a888e43a23840e1cceb8
lefranni <nicolas.lefranc1@etu.univ-grenoble-alpes.fr>
2022-02-07 10:56:13 +0100

    patate

--- personnages/patate.py
+++ personnages/patate.py
@@ -3,7 +3,7 @@ import pygame
 class patate():
     def __init__(self, screen):
         self.screen = screen
-        self.image = pygame.image.load("images/patate.png")
+        self.image = pygame.image.load("../images/patate.png")
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
