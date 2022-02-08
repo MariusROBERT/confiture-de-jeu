d09b87cd873d52389d74fb1a487fb3490a6bbabd
lefranni <nicolas.lefranc1@etu.univ-grenoble-alpes.fr>
2022-02-08 14:26:02 +0100

    point cardinaux patate

--- personnages/potatoes.py
+++ personnages/potatoes.py
@@ -17,21 +17,29 @@ class Potatoes:
                 y_pousse = random.randint(0, NB_ELEM_Y)
                 self.__pos_pousse = x_pousse * CASE_SIZE, y_pousse * CASE_SIZE
 
-        x_patate = randrange(x_pousse - 1, x_pousse + 2)
-        y_patate = randrange(y_pousse - 1, y_pousse + 2)
-        self.__pos_patate = x_patate * CASE_SIZE, y_patate * CASE_SIZE
+        def position(self):
+            #x_patate = randrange(x_pousse - 1, x_pousse + 2)
+            #y_patate = randrange(y_pousse - 1, y_pousse + 2)
+            x_patate=random.randint(x_pousse-1,x_pousse+1)
+            if x_patate!=0 :
+                y_patate=y_pousse
+            else:
+                y_patate=random.randint(y_pousse-1,y_pousse+1)
+            self.__pos_patate = x_patate * CASE_SIZE, y_patate * CASE_SIZE
+            return self.__pos_patate
+
+        position(self)
 
         for i in TOURS:
             while (self.__pos_patate[0]== i[0] and self.__pos_patate[1]==i[1]) or (self.__pos_patate[0] > (SIZE[0]-CASE_SIZE) or self.__pos_patate[0]<0) or (self.__pos_patate[1] > (SIZE[1]-CASE_SIZE) or self.__pos_patate[1]<0):
-                x_patate = randrange(x_pousse - 1, x_pousse + 2)
-                y_patate = randrange(y_pousse - 1, y_pousse + 2)
-                self.__pos_patate = x_patate * CASE_SIZE, y_patate * CASE_SIZE
+                position(self)
 
         self.image_patate = pygame.image.load("./images/patate.png")
         self.image_pousse = pygame.image.load("./images/pousse_patate.png")
         self.size = CASE_SIZE
         self.age = 0
         self.lifespan = 26
+
 
     @property
     def alive(self):
