78cfe81bfaa0810497a0c3c93518ab41bf3321ac
Marius ROBERT <mariusrobert380@gmail.com>
2022-02-09 16:01:40 +0100

    hurt sound and don't crash if a sound is missing

--- py_sounds.py
+++ py_sounds.py
@@ -1,54 +1,53 @@
 import pygame
-
+from personnages.autre_element.fx_manager import DAMAGE_EVENT
 from constantes import DATAPACK
 
 pygame.init()
 
+def get_sound(sound_name: str) -> pygame.mixer.Sound:
+    try:
+        sound = pygame.mixer.Sound(base_path_sounds + sound_name + ".ogg")
+    except FileNotFoundError:
+        try:
+            sound = pygame.mixer.Sound(base_path_sounds + sound_name + ".wav")
+        except FileNotFoundError:
+            return None
+    return sound
+
 base_path_sounds = "./datapacks/" + DATAPACK + "/sounds/"
 # EVENT DE 10 A 20 reservés
 COLLECT_POTATOE = pygame.USEREVENT + 10
-try:
-    collect_sound = pygame.mixer.Sound(base_path_sounds + "collect.ogg")
-except FileNotFoundError:
-    collect_sound = pygame.mixer.Sound(base_path_sounds + "collect.wav")
+collect_sound = get_sound("collect")
 
 DEAD_ZOMBIE = pygame.USEREVENT + 11
-try:
-    dead_sound = pygame.mixer.Sound(base_path_sounds + "dead_zombie.wav")
-except FileNotFoundError:
-    dead_sound = pygame.mixer.Sound(base_path_sounds + "dead_zombie.ogg")
+dead_sound = get_sound("dead_zombie")
 
 FEEDED = pygame.USEREVENT + 12
-try:
-    feed_sound = pygame.mixer.Sound(base_path_sounds + "feeded.wav")
-except FileNotFoundError:
-    feed_sound = pygame.mixer.Sound(base_path_sounds + "feeded.ogg")
+feed_sound = get_sound("feeded")
 
 OUT_OF_FOOD = pygame.USEREVENT + 13
-try:
-    out_of_food_sound = pygame.mixer.Sound(base_path_sounds + "out_of_food.wav")
-except FileNotFoundError:
-    out_of_food_sound = pygame.mixer.Sound(base_path_sounds + "out_of_food.ogg")
+out_of_food_sound = get_sound("out_of_food")
 
 DIG = pygame.USEREVENT + 14
-try:
-    dig_sound = pygame.mixer.Sound(base_path_sounds + "dig.wav")
-except FileNotFoundError:
-    dig_sound = pygame.mixer.Sound(base_path_sounds + "dig.ogg")
+dig_sound = get_sound("dig")
 
+hurt_sound = get_sound("hurt")
 
 def sound_manager(pygame, event: pygame.event.Event):
     try:
         if event.type == COLLECT_POTATOE:
             collect_sound.play().set_volume(0.3)
-        if event.type == DEAD_ZOMBIE:
+        elif event.type == DEAD_ZOMBIE:
             dead_sound.play().set_volume(0.06)
-        if event.type == FEEDED:
+        elif event.type == FEEDED:
             feed_sound.play().set_volume(0.3)
-        if event.type == OUT_OF_FOOD:
+        elif event.type == OUT_OF_FOOD:
             out_of_food_sound.play().set_volume(0.3)
-        if event.type == DIG:
+        elif event.type == DIG:
             dig_sound.play().set_volume(0.3)
+        elif event.type == DAMAGE_EVENT:
+            hurt_sound.play().set_volume(0.3)
+            # print("DAMAGE_EVENT")
 
     except Exception as e:
         print(e)
